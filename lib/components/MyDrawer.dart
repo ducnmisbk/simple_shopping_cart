19d7def9c21b25293a9dcd0bc7e0f0084edcc7ce
ducnm.isbk <ducnm.isbk@gmail.com>
2022-02-17 11:32:10 +0700

    Add customer drawer

---
+++ lib/components/MyDrawer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.blueAccent,
+      child: SafeArea(
+        child: Theme(
+          data: ThemeData(brightness: Brightness.dark),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: const [
+              ListTile(
+                leading: Icon(Icons.new_releases),
+                title: Text('News'),
+              ),
+              ListTile(
+                leading: Icon(Icons.star),
+                title: Text('Favourites'),
+              ),
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Map'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Profile'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/catalog.dart
+++ lib/screens/catalog.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_shopping_cart/components/MyDrawer.dart';
 import 'package:simple_shopping_cart/models/Cart.dart';
 import 'package:simple_shopping_cart/models/Product.dart';
 import 'package:simple_shopping_cart/models/User.dart';
@@ -16,10 +17,24 @@ class CatalogScreen extends StatefulWidget {
   _CatalogScreenState createState() => _CatalogScreenState();
 }
 
-class _CatalogScreenState extends State<CatalogScreen> {
+class _CatalogScreenState extends State<CatalogScreen>
+    with SingleTickerProviderStateMixin {
+  static const Duration toggleDuration = Duration(milliseconds: 250);
+  static const double maxSlide = 225;
+  static const double minDragStartEdge = 60;
+  static const double maxDragStartEdge = maxSlide - 16;
+  bool _canBeDragged = false;
+  late AnimationController _animationController;
+
   @override
   void initState() {
     super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: toggleDuration,
+    );
+
     final products = Provider.of<ProductProvider>(context, listen: false);
     try {
       EasyLoading.show(
@@ -32,8 +47,48 @@ class _CatalogScreenState extends State<CatalogScreen> {
     }
   }
 
+  void close() => _animationController.reverse();
+
+  void open() => _animationController.forward();
+
+  void toggleDrawer() => _animationController.isCompleted ? close() : open();
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _toggle,
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, _) {
+          double slide = maxSlide * _animationController.value;
+          double scale = 1 - (_animationController.value * 0.3);
+          return Stack(
+            children: <Widget>[
+              const MyDrawer(),
+              Transform(
+                transform: Matrix4.identity()
+                  ..translate(slide)
+                  ..scale(scale),
+                alignment: Alignment.centerLeft,
+                child: buildMainCatalog(context),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildMainCatalog(BuildContext context) {
     final products = Provider.of<ProductProvider>(context);
     return Scaffold(
       body: CustomScrollView(
@@ -56,6 +111,45 @@ class _CatalogScreenState extends State<CatalogScreen> {
       ),
     );
   }
+
+  void _onDragStart(DragStartDetails details) {
+    bool isDragOpenFromLeft = _animationController.isDismissed &&
+        details.globalPosition.dx < minDragStartEdge;
+    bool isDragCloseFromRight = _animationController.isCompleted &&
+        details.globalPosition.dx > maxDragStartEdge;
+
+    _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (_canBeDragged) {
+      double delta = details.primaryDelta! / maxSlide;
+      _animationController.value += delta;
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    //I have no idea what it means, copied from Drawer
+    double _kMinFlingVelocity = 365.0;
+
+    if (_animationController.isDismissed || _animationController.isCompleted) {
+      return;
+    }
+    if (details.velocity.pixelsPerSecond.dx.abs() >= _kMinFlingVelocity) {
+      double visualVelocity = details.velocity.pixelsPerSecond.dx /
+          MediaQuery.of(context).size.width;
+
+      _animationController.fling(velocity: visualVelocity);
+    } else if (_animationController.value < 0.5) {
+      close();
+    } else {
+      open();
+    }
+  }
+
+  void _toggle() => _animationController.isDismissed
+      ? _animationController.forward()
+      : _animationController.reverse();
 }
 
 class CatalogAppBar extends StatelessWidget {
